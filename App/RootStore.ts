0c582c4ec141e9dd23a5dadf3603c09e86d04604
reujab <reujab@gmail.com>
2020-03-29 22:10:28 -0400

    added mobx-sync

--- App/RootStore.ts
+++ App/RootStore.ts
@@ -1,10 +1,12 @@
 import { observable } from "mobx"
 import Set from "./Set"
+import { ignore } from "mobx-sync"
 
 export default class RootStore {
 	@observable
 	sets: Set[] = []
 
 	@observable
+	@ignore
 	selectedSet: Set | null = null
 }
--- App/index.tsx
+++ App/index.tsx
@@ -2,14 +2,36 @@ import "react-native-gesture-handler"
 
 import React from "react"
 import Root from "./Root"
-import context from "./context"
 import RootStore from "./RootStore"
+import context from "./context"
+import { AsyncStorage } from "react-native"
+import { AsyncTrunk } from "mobx-sync"
+import { observable } from "mobx"
+import { observer } from "mobx-react"
 
+@observer
 export default class App extends React.Component {
+	store = new RootStore()
+
+	@observable
+	initialized = false
+
+	constructor(props) {
+		super(props)
+
+		const trunk = new AsyncTrunk(this.store, {
+			storage: AsyncStorage,
+		})
+
+		trunk.init().then(() => {
+			this.initialized = true
+		})
+	}
+
 	render(): JSX.Element {
 		return (
-			<context.Provider value={new RootStore()}>
-				<Root />
+			<context.Provider value={this.store}>
+				{this.initialized && <Root />}
 			</context.Provider>
 		)
 	}
