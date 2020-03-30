225e178070c27f91ddda6808153021f452baccb2
reujab <reujab@gmail.com>
2020-03-30 17:28:32 -0400

    improved style of EditSetScreen

---
+++ App/EditSetScreen/AddButton/index.tsx
@@ -0,0 +1,27 @@
+import React from "react"
+import styles from "../../styles"
+import { TouchableOpacity } from "react-native"
+import Icon from "react-native-vector-icons/Ionicons"
+
+export default class AddButton extends React.Component<{
+	onPress: any
+}> {
+	render(): JSX.Element {
+		return (
+			<TouchableOpacity
+				onPress={this.props.onPress}
+				style={[styles.shadow, {
+					alignItems: "center",
+					aspectRatio: 1,
+					backgroundColor: "#ccc",
+					borderRadius: 32,
+					justifyContent: "center",
+					margin: 4,
+					width: 64,
+				}]}
+			>
+				<Icon name="ios-add" size={32} />
+			</TouchableOpacity>
+		)
+	}
+}
--- App/EditSetScreen/index.tsx
+++ App/EditSetScreen/index.tsx
@@ -1,9 +1,12 @@
+import AddButton from "./AddButton"
 import Card from "../Card"
 import React from "react"
 import RootStore from "../RootStore"
 import context from "../context"
-import { ScrollView, Button, View } from "react-native"
-import { TextInput } from "react-native-gesture-handler"
+import styles from "../styles"
+import { Input } from "react-native-elements"
+import { KeyboardAwareScrollView } from "react-native-keyboard-aware-scroll-view"
+import { Platform, View } from "react-native"
 import { observer } from "mobx-react"
 
 @observer
@@ -14,21 +17,40 @@ export default class EditSetScreen extends React.Component {
 
 	render(): JSX.Element {
 		return (
-			<ScrollView>
+			<KeyboardAwareScrollView
+				enableOnAndroid
+				contentContainerStyle={{ flexGrow: 1 }}
+				extraScrollHeight={Platform.OS === "android" ? 100 : 0}
+			>
 				{this.context.selectedSet.cards.map((card) => (
-					<View key={card.id}>
-						<TextInput
-							placeholder="Front"
-							onChangeText={(text): void => { card.front = text }}
-						/>
-						<TextInput
-							placeholder="Back"
-							onChangeText={(text): void => { card.back = text }}
-						/>
+					<View
+						key={card.id}
+						style={[styles.shadow, {
+							borderRadius: 4,
+							margin: 20,
+							marginBottom: 0,
+							padding: 20,
+						}]}
+					>
+						<View>
+							<Input
+								label="Front"
+								defaultValue={card.front}
+								onChangeText={(text): void => { card.front = text }}
+								containerStyle={{ marginBottom: 20 }}
+							/>
+							<Input
+								label="Back"
+								defaultValue={card.back}
+								onChangeText={(text): void => { card.back = text }}
+							/>
+						</View>
 					</View>
 				))}
-				<Button title="+" onPress={this.addCard.bind(this)} />
-			</ScrollView>
+				<View style={{ alignSelf: "center", marginVertical: 20 }}>
+					<AddButton onPress={(): void => this.addCard()} />
+				</View>
+			</KeyboardAwareScrollView>
 		)
 	}
 
