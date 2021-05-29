0896cd8bf5d11b8b36de8836546b0828cd2d0fbf
reujab <reujab@gmail.com>
2021-05-28 21:54:26 -0400

    added new card animation

---
+++ App/EditSetScreen/EditableCard/index.tsx
@@ -0,0 +1,194 @@
+import * as Haptics from "expo-haptics"
+import * as ImagePicker from "expo-image-picker"
+import Flashcard from "../../Flashcard"
+import Icon from "react-native-vector-icons/FontAwesome5"
+import React from "react"
+import commonStyles from "../../commonStyles"
+import { Animated, Image, StyleSheet, Text, TouchableWithoutFeedback, View, TouchableOpacity, Clipboard } from "react-native"
+import { Side } from "../side"
+import { TextInput } from "react-native-paper"
+import { observer } from "mobx-react"
+import { useActionSheet } from "@expo/react-native-action-sheet"
+
+const styles = StyleSheet.create({
+	card: {
+		backgroundColor: "white",
+		borderRadius: 4,
+		margin: 20,
+		marginBottom: 0,
+		padding: 20,
+	},
+	buttons: { flexDirection: "row" },
+	button: {
+		alignItems: "center",
+		flex: 1,
+		justifyContent: "center",
+	},
+	image: {
+		height: 32,
+		width: 32,
+	},
+	addButtonWrapper: {
+		alignSelf: "center",
+		marginVertical: 20,
+	},
+	addButton: {
+		aspectRatio: 1,
+		justifyContent: "center",
+	},
+})
+
+@observer
+class EditableCard extends React.Component<{
+		actionSheet: any
+		card: Flashcard
+		addInput: (ref: React.Component, id: string, side: Side) => void
+		focusNextInput: (card: Flashcard, side: Side) => void
+	}> {
+	scale = new Animated.Value(0)
+
+	componentDidMount(): void {
+		Animated.spring(this.scale, {
+			toValue: 1,
+			speed: 50,
+			bounciness: 8,
+			useNativeDriver: true,
+		}).start()
+	}
+
+	render(): JSX.Element {
+		const { card } = this.props
+
+		return (
+			<Animated.View
+				key={card.id}
+				style={[{ transform: [{ scaleY: this.scale }] }, commonStyles.shadow, styles.card]}
+			>
+				<TextInput
+					ref={(ref): void => { this.props.addInput(ref, card.id, Side.front) }}
+					mode="outlined"
+					label="Front"
+					defaultValue={card.front}
+					onChangeText={(text): void => { card.front = text }}
+					returnKeyType="next"
+					blurOnSubmit={false}
+					onSubmitEditing={(): void => { this.props.focusNextInput(card, Side.front) }}
+					autoCapitalize="none"
+					style={{ marginBottom: 20 }}
+				/>
+				<TextInput
+					ref={(ref): void => { this.props.addInput(ref, card.id, Side.back) }}
+					mode="outlined"
+					label="Back"
+					defaultValue={card.back}
+					onChangeText={(text): void => { card.back = text }}
+					returnKeyType="next"
+					blurOnSubmit={false}
+					onSubmitEditing={(): void => { this.props.focusNextInput(card, Side.back) }}
+					autoCapitalize="none"
+					style={{ marginBottom: 20 }}
+				/>
+				<TextInput
+					mode="outlined"
+					multiline
+					label="Example"
+					defaultValue={card.example}
+					onChangeText={(text): void => { card.example = text }}
+					blurOnSubmit={false}
+					autoCapitalize="none"
+					style={{ marginBottom: 20 }}
+				/>
+				<View style={styles.buttons}>
+					<View style={styles.button}>
+						<TouchableWithoutFeedback onPress={(): void => { EditableCard.master(card) }}>
+							<Icon
+								name="star"
+								color="#ff9800"
+								size={32}
+								solid={card.mastered}
+							/>
+						</TouchableWithoutFeedback>
+						<Text>Mastered</Text>
+					</View>
+					<View style={styles.button}>
+						<TouchableOpacity onPress={(): void => { this.addImage(card) }}>
+							{card.image ? (
+								<Image
+									source={{ uri: card.image }}
+									style={styles.image}
+								/>
+							) : (
+								<Icon
+									name="image"
+									size={32}
+								/>
+							)}
+						</TouchableOpacity>
+						<Text>Image</Text>
+					</View>
+				</View>
+			</Animated.View>
+		)
+	}
+
+	static master(card: Flashcard): void {
+		card.mastered = !card.mastered
+		Haptics.impactAsync()
+	}
+
+	static async pickImage(): Promise<null | string> {
+		const result: any = await ImagePicker.launchImageLibraryAsync({
+			allowsEditing: true,
+			quality: 0,
+			base64: true,
+		})
+
+		if (result.cancelled) {
+			return null
+		}
+
+		// eslint-disable-next-line require-atomic-updates
+		return `data:image/png;base64,${result.base64}`
+	}
+
+	addImage(card: Flashcard): void {
+		const options = [
+			"Cancel",
+			"Paste",
+			card.image ? "Replace" : "Add",
+		]
+		if (card.image) {
+			options.push("Delete")
+		}
+
+		this.props.actionSheet.showActionSheetWithOptions({
+			options,
+			cancelButtonIndex: 0,
+		}, async (index) => {
+			switch (index) {
+				// Paste
+				case 1: {
+					card.image = await Clipboard.getString()
+					break
+				}
+
+				// Replace
+				case 2: {
+					const image = await EditableCard.pickImage()
+					if (image) {
+						card.image = image
+					}
+					break
+				}
+
+				// Delete
+				case 3: {
+					card.image = null
+					break
+				}
+			}
+		})
+	}
+}
+
+export default (props): JSX.Element => <EditableCard actionSheet={useActionSheet()} {...props} />
--- App/EditSetScreen/index.tsx
+++ App/EditSetScreen/index.tsx
@@ -1,5 +1,4 @@
-import * as Haptics from "expo-haptics"
-import * as ImagePicker from "expo-image-picker"
+import EditableCard from "./EditableCard"
 import Flashcard from "../Flashcard"
 import Icon from "react-native-vector-icons/FontAwesome5"
 import Languages from "./Languages"
@@ -8,16 +7,10 @@ import RootStore from "../RootStore"
 import commonStyles from "../commonStyles"
 import context from "../context"
 import { FAB, TextInput } from "react-native-paper"
-import { Image, Platform, Text, TouchableWithoutFeedback, View, TouchableOpacity, StyleSheet, Clipboard } from "react-native"
 import { KeyboardAwareScrollView } from "react-native-keyboard-aware-scroll-view"
+import { Platform, Text, TextInput as NativeTextInput, View, StyleSheet } from "react-native"
+import { Side } from "./side"
 import { observer } from "mobx-react"
-import { useActionSheet } from "@expo/react-native-action-sheet"
-
-// eslint-disable-next-line no-shadow
-enum Input {
-	front,
-	back,
-}
 
 const styles = StyleSheet.create({
 	card: {
@@ -44,9 +37,7 @@ const styles = StyleSheet.create({
 })
 
 @observer
-class EditSetScreen extends React.Component<{
-		actionSheet: any
-	}> {
+export default class EditSetScreen extends React.Component {
 	static contextType = context
 
 	context: RootStore
@@ -101,71 +92,12 @@ class EditSetScreen extends React.Component<{
 					</View>
 				</View>
 				{this.context.selectedSet.cards.map((card) => (
-					<View key={card.id} style={[commonStyles.shadow, styles.card]}>
-						<TextInput
-							ref={(ref): void => { this.addInput(ref, card.id, Input.front) }}
-							mode="outlined"
-							label="Front"
-							defaultValue={card.front}
-							onChangeText={(text): void => { card.front = text }}
-							returnKeyType="next"
-							blurOnSubmit={false}
-							onSubmitEditing={(): void => { this.focusNextInput(card, Input.front) }}
-							autoCapitalize="none"
-							style={{ marginBottom: 20 }}
-						/>
-						<TextInput
-							ref={(ref): void => { this.addInput(ref, card.id, Input.back) }}
-							mode="outlined"
-							label="Back"
-							defaultValue={card.back}
-							onChangeText={(text): void => { card.back = text }}
-							returnKeyType="next"
-							blurOnSubmit={false}
-							onSubmitEditing={(): void => { this.focusNextInput(card, Input.back) }}
-							autoCapitalize="none"
-							style={{ marginBottom: 20 }}
-						/>
-						<TextInput
-							mode="outlined"
-							multiline
-							label="Example"
-							defaultValue={card.example}
-							onChangeText={(text): void => { card.example = text }}
-							blurOnSubmit={false}
-							autoCapitalize="none"
-							style={{ marginBottom: 20 }}
-						/>
-						<View style={styles.buttons}>
-							<View style={styles.button}>
-								<TouchableWithoutFeedback onPress={(): void => { EditSetScreen.master(card) }}>
-									<Icon
-										name="star"
-										color="#ff9800"
-										size={32}
-										solid={card.mastered}
-									/>
-								</TouchableWithoutFeedback>
-								<Text>Mastered</Text>
-							</View>
-							<View style={styles.button}>
-								<TouchableOpacity onPress={(): void => { this.addImage(card) }}>
-									{card.image ? (
-										<Image
-											source={{ uri: card.image }}
-											style={styles.image}
-										/>
-									) : (
-										<Icon
-											name="image"
-											size={32}
-										/>
-									)}
-								</TouchableOpacity>
-								<Text>Image</Text>
-							</View>
-						</View>
-					</View>
+					<EditableCard
+						key={card.id}
+						card={card}
+						addInput={this.addInput.bind(this)}
+						focusNextInput={this.focusNextInput.bind(this)}
+					/>
 				))}
 				<View style={styles.addButtonWrapper}>
 					{/* eslint-disable-next-line react/jsx-pascal-case */}
@@ -187,15 +119,15 @@ class EditSetScreen extends React.Component<{
 		this.context.selectedSet.cards.push(new Flashcard())
 	}
 
-	addInput(ref: any, id: string, input: Input): void {
+	addInput(ref: any, id: string, side: Side): void {
 		this.inputs[id] = {
 			...this.inputs[id],
-			[input]: ref,
+			[side]: ref,
 		}
 	}
 
-	focusNextInput(card: Flashcard, input: number): void {
-		if (input === Input.back) {
+	focusNextInput(card: Flashcard, side: Side): void {
+		if (side === Side.back) {
 			const index = this.context.selectedSet.cards.indexOf(card) + 1
 			if (index in this.context.selectedSet.cards) {
 				const id = this.context.selectedSet.cards[index].id
@@ -203,71 +135,10 @@ class EditSetScreen extends React.Component<{
 			} else {
 				this.addCard()
 				// gives time for ref to initialize
-				setImmediate(() => this.focusNextInput(card, input))
+				setImmediate(() => this.focusNextInput(card, side))
 			}
 		} else {
-			this.inputs[card.id][input + 1].focus()
+			this.inputs[card.id][side + 1].focus()
 		}
-	}
-
-	addImage(card: Flashcard): void {
-		const options = [
-			"Cancel",
-			"Paste",
-			card.image ? "Replace" : "Add",
-		]
-		if (card.image) {
-			options.push("Delete")
-		}
-
-		this.props.actionSheet.showActionSheetWithOptions({
-			options,
-			cancelButtonIndex: 0,
-		}, async (index) => {
-			switch (index) {
-				// Paste
-				case 1: {
-					card.image = await Clipboard.getString()
-					break
-				}
-
-				// Replace
-				case 2: {
-					const image = await EditSetScreen.pickImage()
-					if (image) {
-						card.image = image
-					}
-					break
-				}
-
-				// Delete
-				case 3: {
-					card.image = null
-					break
-				}
-			}
-		})
-	}
-
-	static async pickImage(): Promise<null | string> {
-		const result: any = await ImagePicker.launchImageLibraryAsync({
-			allowsEditing: true,
-			quality: 0,
-			base64: true,
-		})
-
-		if (result.cancelled) {
-			return null
-		}
-
-		// eslint-disable-next-line require-atomic-updates
-		return `data:image/png;base64,${result.base64}`
-	}
-
-	static master(card: Flashcard): void {
-		card.mastered = !card.mastered
-		Haptics.impactAsync()
 	}
 }
-
-export default (props): JSX.Element => <EditSetScreen actionSheet={useActionSheet()} {...props} />
